027c0f69d94a8be135872868d34eb3208c832385
Adam Fraser <AJFraser10@GMail.com>
2016-08-16 14:16:35 -0500

    Completed js-transducer-assignment

--- lib.js
+++ lib.js
@@ -19,13 +19,50 @@ function item(name, price) {
  * @param endTime a Number representing an ending time.
  * @param customers an array of Customer objects.
  * @returns An Object of item names mapped to the total sales of that item
- *  in the given State and between the given startTime and endTime.   
+ *  in the given State and between the given startTime and endTime.
  */
 function grossPerItemInState(state, startTime, endTime, customers) {
-  //TODO: Make this actually calculate values.
-  return {
-    wizz: 600
-    bang: 300
+
+  const itemNames = {}
+
+    customers.filter((customer) => {return(customer.state === state)})
+        .forEach((customer) => {customer.orders
+          .filter((orders) => {return (orders.timeStamp > startTime && orders.timeStamp < endTime)})
+        .forEach((orders) => {orders.items
+        .forEach((items) => {if(itemNames[items.name] === undefined){
+          itemNames[items.name] = 0}itemNames[items.name]+=items.price
+        })})})
+        
+
+        // customers.filter((customer) => {return(customer.state === state)})
+        //     .forEach((customer) => {customer.orders
+        //       .filter((orders) => {return (orders.timeStamp > startTime && orders.timeStamp < endTime)})
+        //     .forEach((orders) => {orders.items
+        //     .forEach((items) => {if(itemNames[items.name] === undefined){
+        //       itemNames[items.name] = 0}itemNames[items.name]+=items.price
+        //     })})})
+
+
+
+    // customers.forEach(function (n) {customers[c.orders.filter((item) => {})] = customer(o, orders)}) //iterates over what is left in map
+    // state.filter(function (orders) {return !== state} ) //iterates over all n
+    // item.map(function (items) {return grossPerItemInState}) //iterates over what is left in filter
+
+    //
+    // for (customers i = 0; i < array.lencustomers; i++) {
+    //   array[i]
+    //   if (condition) {
+    //
+    //   }
+    // }
+    // if (timeStamp > startTime && timeStamp < endTime) {
+    //   customers.reduce((sum, price) => {next, ...sum}, [])
+    // }
+
+
+  return { itemNames
+    // wizz: 600
+    // bang: 300
   }
 }
 
